b4e54fb81b26b349526509fcd35f7fe0bff2283b
Elyes.Benkhoud <elyesbenkhoud377@gmail.com>
2023-01-11 12:50:52 +0200

    apolo client setup front 1.1v

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles';
-import Pages from './pages';
+import React from "react";
+import ReactDOM from "react-dom";
+import GlobalStyles from "./styles";
+import Pages from "./pages";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
+const client = new ApolloClient({
+  uri: " http://localhost:4000",
+  cache: new InMemoryCache(),
+  //every instance of ApolloClient uses an in-memory cache.
+  //This enables it to store and reuse query results so it doesn't have to make as many network requests.
+  // This makes our app's user experience feel much snappier.
+});
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById("root")
 );
