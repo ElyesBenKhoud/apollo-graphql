25549bf305a9e2beb5269d9a18487a4386cb1d96
Elyes.Benkhoud <elyesbenkhoud377@gmail.com>
2023-01-13 10:02:28 +0200

    update schema : card details

--- server/src/schema.js
+++ server/src/schema.js
@@ -18,6 +18,19 @@ const typeDefs = gql`
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
+    "The track's complete description, can be in Markdown format"
+    description: String
+    "The number of times a track has been viewed"
+    numberOfViews: Int
+    "The track's complete array of Modules"
+    modules: [Module!]!
+  }
+  type Module {
+    id: ID!
+    "The Module's title"
+    title: String!
+    "The Module's length in minutes"
+    length: Int
   }
   "Author of a complete Track or a Module"
   type Author {
