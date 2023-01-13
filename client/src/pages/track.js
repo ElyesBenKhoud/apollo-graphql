67c0f5eafd8e6cd3acd77cbec7b30d82f5bda9e8
Elyes.Benkhoud <elyesbenkhoud377@gmail.com>
2023-01-13 11:59:47 +0200

    trackDetail displayed & API called implemented

---
+++ client/src/pages/track.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import { Layout, QueryResult } from "../components";
+import TrackDetail from "../components/track-detail";
+export const GET_TRACK = gql`
+  query GetTrack($trackId: ID!) {
+    track(id: $trackId) {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+      numberOfViews
+      modules {
+        id
+        title
+        length
+      }
+      description
+    }
+  }
+`;
+const Track = ({ trackId }) => {
+  const { loading, error, data } = useQuery(GET_TRACK, {
+    variables: { trackId },
+  });
+  return (
+    <Layout>
+      <QueryResult error={error} data={data} loading={loading}>
+        <TrackDetail track={data?.track} />
+      </QueryResult>
+    </Layout>
+  );
+};
+
+export default Track;
