3b28c5e52b4d8052771070f283bc878402ddaa82
Elyes.Benkhoud <elyesbenkhoud377@gmail.com>
2023-01-16 15:12:16 +0200

    mutation with errors

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -14,13 +14,22 @@ const resolvers = {
   Mutation: {
     // increments a track's numberOfViews property
     incrementTrackViews: async (_, { id }, { dataSources }) => {
-      const track = await dataSources.trackAPI.incrementTrackViews(id);
-      return {
-        code: 200,
-        success: true,
-        message: `Successfully incremented number of views for track ${id}`,
-        track,
-      };
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track,
+        };
+      } catch (err) {
+        return {
+          code: err.extensions.response.status,
+          success: false,
+          message: err.extensions.response.body,
+          track: null,
+        };
+      }
     },
   },
 
